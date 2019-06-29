344d4f8e91694620a0b0f58b0196b8914f783a90
Rudy Bouset <bousset.rudy@gmail.com>
2019-06-29 03:48:16 +0200

    Additions to my dwm status bar

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+cd ~/
 compton &
 st -e htop &
 sleep 0.5s
 st -e ranger &
 sleep 0.5s
 st &
+st -e neomutt &
 
 dte(){
 	dte="$(date +"%H:%M")"
@@ -17,7 +19,23 @@ bat(){
 	echo -e "BAT: $status: $percentage"
 }
 
+tem(){
+	tem="$(sensors | grep 'id' | awk '{print $4}' | cut -c 2-)"
+	echo -e "TEMP: $tem"
+}
+
+ips(){
+	ip="$(ip -h address show | grep '192.168' | awk '{print $2}' | rev | cut -c 4- | rev)"
+	echo -e "IP: $ip"
+}
+
+dsk(){
+	dsk_root="$(df -h | grep '/dev/sda7' | awk '{print $4}')"
+	dsk_home="$(df -h | grep '/dev/sda8' | awk '{print $4}')"
+	echo -e "ROOT: $dsk_root - HOME: $dsk_home"
+}
+
 while true; do
-	xsetroot -name "$(bat) | $(dte)"
+	xsetroot -name " $(tem) | $(dsk) | $(ips) | $(bat) | $(dte)"
 	sleep 20s # Update time every minute
 done &
