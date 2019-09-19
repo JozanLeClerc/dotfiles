979b5d3b0a1368ad9a6017292dfaf39a3a06542b
Rudy Bousset <bousset.rudy@gmail.com>
2019-09-19 17:46:00 +0200

    better identation, changed super+tab hotkey to alt+tab

--- 2-laptop/dwm/config.h
+++ 2-laptop/dwm/config.h
@@ -30,21 +30,21 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      		instance    		title       	tags mask     isfloating   monitor */
-	{ "Gimp",     		NULL,       		NULL,       	0,            1,           -1 },
-	{ "Tor Browser",	"TorLauncher",	        NULL,       	0,            1,           -1 },
-	{ "Tor Browser",	"Navigator",	        NULL,       	1 << 2,       0,           -1 },
-	{ "Firefox",  		NULL,       		NULL,       	1 << 2,       0,           -1 },
-	{ "trayer",   		NULL,       		NULL,       	1 << 8,       0,           -1 },
-	{ "Surf",    		NULL,       		NULL,       	1 << 2,       0,           -1 },
-	{ "tabbed",   		NULL,       		NULL,       	1 << 2,       0,           -1 },
-	{ "jetbrains-studio",   NULL,       		NULL,       	1 << 1,       1,           -1 },
-	{ "trayer",		NULL,       		NULL,       	1 << 9,       0,           -1 },
-	{ NULL,	      		NULL,       		"neomutt",  	1 << 8,       0,           -1 },
-	{ NULL,	      		NULL,       		"ncmpcpp", 	1 << 7,       0,           -1 },
-	{ NULL,	      		NULL, 		        "calcurse", 	1 << 6,       0,           -1 },
-	{ NULL,	     	 	NULL,       		"newsboat", 	1 << 5,       0,           -1 },
-	{ NULL,	     	 	NULL,       		"pulsemixer", 	1 << 4,       0,           -1 },
+	/* class      					instance    			title       	tags mask     isfloating   monitor */
+	{ "Gimp",     					NULL,       			NULL,       	0,            1,           -1 },
+	{ "Tor Browser",				"TorLauncher",	  NULL,       	0,            1,           -1 },
+	{ "Tor Browser",				"Navigator",	    NULL,       	1 << 2,       0,           -1 },
+	{ "Firefox",  					NULL,       			NULL,       	1 << 2,       0,           -1 },
+	{ "trayer",   					NULL,       			NULL,       	1 << 8,       0,           -1 },
+	{ "Surf",    						NULL,      				NULL,       	1 << 2,       0,           -1 },
+	{ "tabbed",   					NULL,       			NULL,       	1 << 2,       0,           -1 },
+	{ "jetbrains-studio",   NULL, 			      NULL,       	1 << 1,       1,           -1 },
+	{ "trayer",							NULL,       			NULL,       	1 << 9,       0,           -1 },
+	{ NULL,	      					NULL,       			"neomutt",  	1 << 8,       0,           -1 },
+	{ NULL,	      					NULL,       			"ncmpcpp", 		1 << 7,       0,           -1 },
+	{ NULL,	      					NULL, 		        "calcurse", 	1 << 6,       0,           -1 },
+	{ NULL,	     	 					NULL,       			"newsboat", 	1 << 5,       0,           -1 },
+	{ NULL,	     	 					NULL,       			"pulsemixer", 1 << 4,       0,           -1 },
 };
 
 /* layout(s) */
@@ -75,40 +75,40 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]   	 = { "st", NULL };
-static const char *qbcmd[]  	 = { "qutebrowser", NULL };
-static const char *lockcmd[]	 = { "slock", NULL };
-static const char *backlinccmd[] = { "xbacklight", "-inc", "10", NULL };
-static const char *backldeccmd[] = { "xbacklight", "-dec", "10", NULL };
-static const char *volupcmd[] 	 = { "amixer", "set", "Master", "5%+", NULL };
-static const char *voldowncmd[]	 = { "amixer", "set", "Master", "5%-", NULL };
-static const char *volmutecmd[]	 = { "amixer", "set", "Master", "toggle", NULL };
-static const char *refbarcmd[]	 = { "refbar", NULL };
+static const char *dmenucmd[]			= { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]   		= { "st", NULL };
+static const char *qbcmd[]  	 		= { "qutebrowser", NULL };
+static const char *lockcmd[]	 		= { "slock", NULL };
+static const char *backlinccmd[] 	= { "xbacklight", "-inc", "10", NULL };
+static const char *backldeccmd[] 	= { "xbacklight", "-dec", "10", NULL };
+static const char *volupcmd[] 	 	= { "amixer", "set", "Master", "5%+", NULL };
+static const char *voldowncmd[]	 	= { "amixer", "set", "Master", "5%-", NULL };
+static const char *volmutecmd[]	 	= { "amixer", "set", "Master", "toggle", NULL };
+static const char *refbarcmd[]	 	= { "refbar", NULL };
 
 #include "movestack.c"
 #include "focusurgent.c"
 #include <X11/XF86keysym.h>
 
 static Key keys[] = {
-	/* modifier             key 		           function         argument */
-	{ MODKEY,          	XK_p,      		   spawn,           {.v = dmenucmd } },
-	{ MODKEY,		XK_Return, 		   spawn,           {.v = termcmd } },
-	{ MODKEY|ShiftMask,	XK_l,	   		   spawn,	    {.v = lockcmd } },
-	{ MODKEY,		XK_3,	   		   spawn,           {.v = qbcmd } },
-	{ MODKEY,               XK_Escape, 		   togglebar,       {0} },
-	{ MODKEY,               XK_j,      		   focusstack,      {.i = +1 } },
-	{ MODKEY,               XK_k,      		   focusstack,      {.i = -1 } },
-	{ MODKEY,               XK_i,      		   incnmaster,      {.i = +1 } },
-	{ MODKEY,               XK_d,      		   incnmaster,      {.i = -1 } },
-	{ MODKEY,               XK_h,      		   setmfact,        {.f = -0.05} },
-	{ MODKEY,               XK_l,      		   setmfact,        {.f = +0.05} },
-	{ MODKEY|ShiftMask,     XK_j,      		   movestack,       {.i = +1 } },
-	{ MODKEY|ShiftMask,     XK_k,      		   movestack,       {.i = -1 } },
-	{ MODKEY|ShiftMask,     XK_Return, 		   zoom,            {0} },
-	{ MODKEY,               XK_Tab,    		   view,            {0} },
-	{ MODKEY,		XK_q,      		   killclient,      {0} },
-	{ MODKEY,	        XK_s,      		   setlayout,       {.v = &layouts[0]} },
+	/* modifier									key								function					argument */
+	{ MODKEY,          					XK_p,							spawn,           	{.v = dmenucmd } },
+	{ MODKEY,										XK_Return, 		   	spawn,           	{.v = termcmd } },
+	{ MODKEY|ShiftMask,					XK_l,							spawn,						{.v = lockcmd } },
+	{ MODKEY,										XK_3,							spawn,           	{.v = qbcmd } },
+	{ MODKEY,               		XK_Escape, 		   	togglebar,       	{0} },
+	{ MODKEY,               		XK_j,      		   	focusstack,      	{.i = +1 } },
+	{ MODKEY,               		XK_k,      		   	focusstack,      	{.i = -1 } },
+	{ MODKEY,               		XK_i,      		   	incnmaster,      	{.i = +1 } },
+	{ MODKEY,               		XK_d,      		   	incnmaster,      	{.i = -1 } },
+	{ MODKEY,               		XK_h,      		   	setmfact,        	{.f = -0.05} },
+	{ MODKEY,               		XK_l,      		   	setmfact,        	{.f = +0.05} },
+	{ MODKEY|ShiftMask,     		XK_j,      		   	movestack,       	{.i = +1 } },
+	{ MODKEY|ShiftMask,     		XK_k,      		   	movestack,       	{.i = -1 } },
+	{ MODKEY|ShiftMask,     		XK_Return, 		   	zoom,            	{0} },
+	{ Mod1Mask,               		XK_Tab,    		   	view,            	{0} },
+	{ MODKEY,										XK_q,      		   	killclient,      	{0} },
+	{ MODKEY,	  					      XK_s,      		   setlayout,       {.v = &layouts[0]} },
 	{ MODKEY,               XK_m,	   		   setlayout,       {.v = &layouts[1]} },
 	{ MODKEY,               XK_f,      		   setlayout,       {.v = &layouts[2]} },
 	{ MODKEY|ShiftMask,     XK_s,      		   setlayout,       {.v = &layouts[3]} },
