3218e3c3991565d4d280c771924a04ca92ee54b1
Rudy Bousset <bousset.rudy@gmail.com>
2019-10-12 03:52:10 +0200

    changed htop for gotop

--- 1-desktop/.dwm/autostart.sh
+++ 1-desktop/.dwm/autostart.sh
@@ -6,11 +6,11 @@ setxkbmap -layout us,fr -option grp:alt_shift_toggle &
 xset r rate 150 80 &
 numlockx on > /dev/null 2>&1
 dunst > /dev/null 2>&1 &
-st -e htop &
-sleep 0.5s
 st -e ranger &
 sleep 0.5s
 st &
+sleep 0.5s
+st -e gotop &
 mpd &
 #mopidy &
 st -e neomutt &
