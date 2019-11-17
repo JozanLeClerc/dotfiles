a89aa01c6ad6efff4d584a1df9039bc9fef6060e
JozanLeClerc <bousset.rudy@gmail.com>
2019-11-17 12:21:19 +0100

    webview now downloads into Downloads, also shellchecked

--- .local/bin/webview.sh
+++ .local/bin/webview.sh
@@ -1,17 +1,17 @@
-#!/bin/zsh
+#!/bin/sh
 
 ext="${1##*.}"
 mpvFiles="mkv mp4 gif" 
 sxivFiles="png jpg jpeg jpe"
 wgetFiles="flac mp3 mp3?source=feed opus pdf doc docx"
 
-cd ~/Téléchargements
+cd ~/Downloads || exit
 
-if echo $sxivFiles | grep -w $ext > /dev/null; then
+if echo "$sxivFiles" | grep -w "$ext" > /dev/null; then
 	nohup sxiv "$1" > /dev/null &
-elif echo $mpvFiles | grep -w $ext > /dev/null; then
+elif echo "$mpvFiles" | grep -w "$ext" > /dev/null; then
 	nohup mpv --loop --quiet "$1" > /dev/null &
-elif echo $wgetFiles | grep -w $ext > /dev/null; then
+elif echo "$wgetFiles" | grep -w "$ext" > /dev/null; then
 	nohup st -e wget "$1" > /dev/null &
 else
 	nohup qutebrowser "$1" > /dev/null &
