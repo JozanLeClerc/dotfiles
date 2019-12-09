8281201765b0a2ef674555233632017115be9e2a
JozanLeClerc <bousset.rudy@gmail.com>
2019-12-09 01:47:38 +0100

    Script to sync my todo list

---
+++ .local/bin/todo-git-updater.sh
@@ -0,0 +1,12 @@
+#!/bin/dash
+
+date=$(date)
+
+while [ 1 ]; do
+	cd /home/jozan/.emacs.d/org/todo || exit 1
+	git add . > /dev/null 2>&1
+	if git commit -m "$date" > /dev/null 2>&1; then
+		git push -u origin master > /dev/null 2>&1
+	fi
+	sleep 10800
+done
